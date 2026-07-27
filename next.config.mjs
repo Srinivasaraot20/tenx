97dfe6f29dc2051c2073c648b5c132cb14d8dde6
Srinivasa Rao Talari <143414405+Srinivasaraot20@users.noreply.github.com>
2026-07-27 15:24:28 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,35 @@ const nextConfig = {
   experimental: {
     optimizeCss: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://www.google-analytics.com https://cdn.jsdelivr.net https://cdn.worldvectorlogo.com; font-src 'self' data: https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; frame-src 'self' https://www.google.com; upgrade-insecure-requests;",
+          },
+        ],
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.alias = {
